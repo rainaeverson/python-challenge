881a7099ab192340da9ca6c10bb18a99adc1f270
rainaeverson <rain.a.everson@gmail.com>
2023-05-16 18:32:02 -0500

    Office hours troubleshooting

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,8 +2,8 @@
 import os
 import csv
 
-#Total number of votes cast
-#Complete list of candidates who received votes
+#Total number of votes cast <<<
+#Complete list of candidates who received votes <<<
 #Percentage of votes each candidate won
 #Total number of votes each candidate won
 #Winner of the election
@@ -15,10 +15,11 @@ import csv
 total_votes = 0
 vote_percents = 0
 candidate_total = 0
+candidate_name = ""
 
 ballot_id = []
 county = []
-candidate = []
+#candidate = []
 candidates = []
 candidate_vote_percent = []
 candidate_vote_total = []
@@ -26,16 +27,67 @@ candidate_vote_total = []
 votes_csv = os.path.join("..", "Resources", "election_data.csv")
 
 with open(votes_csv) as vote_file:
-    vote_reader = csv.reader(votes_csv)
+    vote_reader = csv.reader(vote_file)
     header = next(vote_reader)
 
-    #total_votes = len(list(vote_file))
-    #print(total_votes)
+    total_votes = len(list(vote_file))
+    
 
-    for row in vote_file:
-        ballot_id.append(vote_file[0])
-        county.append(vote_file[1])
-        candidate.append(vote_file[2])
+    for row in vote_reader:
+        print(candidates[-1])
+        if len(candidates) == 0:
+            candidates.append(row[2]) 
+        elif (row[2]) != candidates[-1]:
+            candidates.append(row[2]) 
+        
 
-        total_votes = len(ballot_id)
-    print(total_votes)
+        #ballot_id.append(row[0])
+        #county.append(row[1])
+        #candidate.append(row[2])
+        
+        #ballot_id = (row[0])
+        #county = (row[1])
+        #candidate = (row[2])
+        #store "location" of empty list here ?
+        #candidate_vote_total.append(vote_reader)
+
+        #total_votes = len(list(ballot_id))
+        #print(total_votes)
+
+        #print("Candidates are:" + ", ".join(candidate for candidate in candidates))
+
+    #List Candidates
+        #i = 0
+    # for i in row:
+
+        # candidates.append(str(candidate[i]))
+
+        # if str(candidate[i]) != str(candidate[i - 1]):
+        #     candidates.append(str(candidate[i]))
+
+            
+
+        #     #candidate_vote_total.append(int(candidate_total)) #???
+        
+        #     candidate_name = str(candidate[i])
+        #     print(f"{candidate_name}")
+        #     candidate_total = 0
+
+        #     #i += 1
+
+        # elif str(candidate[i]) == str(candidate[i - 1]):
+        #     candidate_total += 1
+
+
+    #     i += 1
+
+    #Percent of votes and total votes for each candidate
+    #o = 0
+    #for o in vote_reader:
+
+        
+        
+#print(f"Total Votes: {total_votes}")
+#print(candidates)
+        
+
